e75685235465e6de909a59b069fa350df92fc89d
Nhan Cao <nhancv92@gmail.com>
2018-03-23 10:13:36 +0700

    add apollo server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,57 @@
-const express = require('express')
-const app = express()
 const path = require('path')
-
+const express = require('express')
+const bodyParser = require('body-parser')
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express')
+const { makeExecutableSchema } = require('graphql-tools')
+const app = express()
 const log = console.log
+
 var port = process.env.PORT || 4000
 
-// Routing
-var frontendDir = path.join(
-  path.dirname(path.dirname(__dirname)),
-  'frontend'
-)
-
-app.use(express.static(path.join(frontendDir, 'build')))
-app.get('/', function(req, res) {
+// For fontend route
+var frontendDir = path.join(path.dirname(path.dirname(__dirname)), 'frontend')
+app.use('/home', express.static(path.join(frontendDir, 'build')))
+app.get('/home', function(req, res) {
   res.sendFile(path.join(frontendDir, 'build', 'index.html'))
 })
+app.get('/', function(req, res) {
+  res.redirect('/home');
+})
+
+// Some fake data
+const books = [
+  {
+    title: "Harry Potter and the Sorcerer's stone",
+    author: 'J.K. Rowling',
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton',
+  },
+];
+
+// The GraphQL schema in string form
+const typeDefs = `
+  type Query { books: [Book] }
+  type Book { title: String, author: String }
+`;
+
+// The resolvers
+const resolvers = {
+  Query: { books: () => books },
+};
+
+// Put together a schema
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+
+// The GraphQL endpoint
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+
+// GraphiQL, a visual editor for queries
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 app.listen(port, function() {
-  log('Server listening at port %d', port)
+  log('Go to http://localhost:%d/graphiql to run queries!', port);
 })
